cf6ddc945ca1617c4eb8434dae46e6c52f5f83c6
rein-viewec <83944723+rein-viewec@users.noreply.github.com>
2022-04-01 15:36:52 +0800

    create project

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// const tabItemSetting = {
+//   slidesPerView: 2,
+//   spaceBetween: 0,
+//   grabCursor: true,
+//   breakpoints: {
+//     '720': {
+//       slidesPerView: 'auto',
+//       spaceBetween: 0,
+//     },
+//   },
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// }
+$(document).ready(() => {
+  // $('.item__tab__new').tab()
+  // $('.item__tab__hot').tab()
+  // AOS.init({
+  //   once: true,
+  // })
+  // new Swiper('.tab__content', tabItemSetting)
+})
